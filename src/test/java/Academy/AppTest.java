269a728928cff557cc74aff6ff0a23ff3e48f72e
Meenal Ahuja <ahuja.meenal3@gmail.com>
2021-04-26 18:23:46 +0530

    did some changes IST zone guy

--- src/test/java/Academy/AppTest.java
+++ src/test/java/Academy/AppTest.java
@@ -22,5 +22,29 @@ public class AppTest
         System.out.println("checking GIT functionalities3");
         System.out.println("checking GIT functionalities4");
         System.out.println("checking GIT functionalities5");
+        
+        
+        System.out.println("checking different branches");
     }
+    
+    
+    @Test
+    public void shouldAnswerWithTrueAgain()
+    {
+        assertTrue( true );
+       
+        System.out.println("checking different branches-hello");
+        System.out.println("checking different branches-how r u");
+    }
+    
+    
+    @Test
+    public void shouldAnswerWithTrueAgainAgain()
+    {
+        assertTrue( true );
+       
+        System.out.println("checking different branches-bbye");
+        System.out.println("checking different branches-oka");
+    }
+    
 }
